52e09e9abaee61242b75d95fb4c14a1d0bc3d224
Mumurilo375 <murilinho.pequeno@gmail.com>
2025-09-26 20:03:52 -0300

    first commit

---
+++ mvc/app/Controller/Pages/Home.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\Controller\Pages;
+
+
+/**
+     * Método responsável por retornar o conteúdo (view) da nossa home
+     * @return string
+     */
+
+
+class Home {
+    public static function getHome() {
+        return 'Home Page';
+    }
+
+}
